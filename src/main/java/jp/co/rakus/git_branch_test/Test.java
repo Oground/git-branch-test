8d0f34f1a6f23e5176d515d422167c3b9c692bfb
rosso357 <hiroshi.igari@rakus.co.jp>
2017-08-24 11:33:44 +0900

    掛け算を行うメソッドを追加しました。

--- src/main/java/jp/co/rakus/git_branch_test/Test.java
+++ src/main/java/jp/co/rakus/git_branch_test/Test.java
@@ -1,17 +1,22 @@
 package jp.co.rakus.git_branch_test;
 
+
 public class Test {
 	public static void main(String[] args) {
 		int num1 = 5;
 		int num2 = 3;
 		
 		int answer = addCalc(num1, num2);
-		
+		int MultiAnswer=multi(num1,num2);
 		System.out.println(answer);
+		System.out.println(MultiAnswer);
 	}
 	
 	static int addCalc(int num1, int num2){
 		return num1 + num2;
 	}
 
+	static int multi(int num1,int num2){
+		return num1*num2;
+	}
 }
